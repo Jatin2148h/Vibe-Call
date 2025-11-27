21b69fe856cdf324082ad077ff46b9df5d1c2748
Jatin Agrawal <agrawaljatin32@gmail.com>
2025-11-27 18:25:04 +0530

    modify

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,12 +18,15 @@ app.set("port", process.env.PORT || 3000);
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
-      "https://vibecallfrontend.onrender.com"
+      "http://localhost:5173",                   // local dev
+      "https://vibecallfrontend.onrender.com",   // deployed frontend
     ],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
+
 
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
